eb13fc367e93f2c9686aca75739e6c36d57e85ba
Alex Brosseau <brosseau.alex@outlook.com>
2025-09-19 13:24:16 -0400

    Ajout de mongo db

--- src/daos/user_dao_mongo.py
+++ src/daos/user_dao_mongo.py
@@ -3,19 +3,42 @@ import os
 
 class UserDAOMongo:
     def __init__(self):
-        host = os.getenv("MONGODB_HOST", "localhost")
-        self.client = MongoClient(f"mongodb://{host}:27017/")
-        self.db = self.client["store_manager"]
-        self.collection = self.db["users"]
+        env_path = "../.env"
+        print(os.path.abspath(env_path))
+        load_dotenv(dotenv_path=env_path)
+        host = os.getenv("MONGODB_HOST", "mongo")
+        db_name = os.getenv("MONGO_DB_NAME", "mydb")
+        username = os.getenv("MONGO_USERNAME", "user")
+        password = os.getenv("MONGO_PASSWORD", "pass")
+        uri = f"mongodb://{username}:{password}@{host}:27017/?authSource=admin"
+        self.client = MongoClient(uri)
+        self.db = self.client[db_name]
+        self.col = self.db["users"]
+    ##-------------Traduction Mongo <-> MySql -------------##
+    def _doc_to_user(self, doc):
+        #transformer doc mongo en User object
+        return User(id=str(doc["_id"]), name=doc.get("name"), email=doc.get("email"))
 
+    def _user_to_doc(self, user: User):
+        # le contraire de l'autre
+        return {"name": user.name, "email": user.email}
+
+    ##-------------    Opération normales    -------------##
     def select_all(self):
-        return list(self.collection.find())
+        docs = self.col.find()
+        return [self._doc_to_user(d) for d in docs]
 
-    def insert(self, user):
-        self.collection.insert_one(user.__dict__)
+    def insert(self, user: User):
+        res = self.col.insert_one(self._user_to_doc(user))
+        return str(res.inserted_id)
 
-    def update(self, user):
-        self.collection.update_one({"id": user.id}, {"$set": user.__dict__})
+    def update(self, user: User):
+        if not user.id:
+            raise ValueError("user.id requis pour update")
+        self.col.update_one(
+            {"_id": ObjectId(user.id)},
+            {"$set": self._user_to_doc(user)}
+        )
 
     def delete(self, user_id):
-        self.collection.delete_one({"id": user_id})
+        self.col.delete_one({"_id": ObjectId(user_id)})
